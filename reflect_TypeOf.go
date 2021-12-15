5896c9f49fd802dc1e5e903a9a50e2f72d8ded2b
x8q7 <924971430@qq.com>
2021-12-15 17:07:45 +0800

    Update reflect_TypeOf.go

--- reflect_TypeOf.go
+++ reflect_TypeOf.go
@@ -5,23 +5,29 @@ import (
 	"reflect"
 )
 
-type Person struct {
-	Name string `json:"name"`
-	Age  int
-}
-
 func main() {
-
-	a := Person{
-		Name: "bob",
-		Age:  18,
+	// 声明一个空结构体
+	type cat struct {
+		Name string
+		// 带有结构体tag的字段
+		Type int `json:"type" id:"100"`
 	}
-
-	rtyp := reflect.TypeOf(a)
-
-	// fmt.Println(rtyp.NumField())
-
-	field1, _ := rtyp.FieldByName("Name")
+	// 创建cat的实例
+	ins := cat{Name: "mimi", Type: 1}
+	// 获取结构体实例的反射类型对象
+	typeOfCat := reflect.TypeOf(ins)
+	// 遍历结构体所有成员
+	for i := 0; i < typeOfCat.NumField(); i++ {
+		// 获取每个成员的结构体字段类型
+		fieldType := typeOfCat.Field(i)
+		// 输出成员名和tag
+		fmt.Printf("name: %v  tag: '%v'\n", fieldType.Name, fieldType.Tag)
+	}
+	// 通过字段名, 找到字段类型信息
+	if catType, ok := typeOfCat.FieldByName("Type"); ok {
+		// 从tag中取出需要的tag
+		fmt.Println(catType.Tag.Get("json"), catType.Tag.Get("id"))
+	}
 
 	/*
 			Name string          // 字段名
@@ -32,12 +38,5 @@ func main() {
 		    Index     []int      // Type.FieldByIndex中的返回的索引值
 		    Anonymous bool       // 是否为匿名字段
 	*/
-
-	fmt.Println(field1.Name)
-	fmt.Println(field1.PkgPath)
-	fmt.Println(field1.Type)
-	fmt.Println(field1.Tag)
-	fmt.Println(field1.Offset)
-	fmt.Println(field1.Index)
-	fmt.Println(field1.Anonymous)
 }
+
